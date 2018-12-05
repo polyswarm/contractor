fa3aaac1f929e4e4c5b5139331f22c64a4dcc77a
Maxwell Koo <mjkoo90@gmail.com>
2018-12-05 15:44:41 -0700

    Only mint on homechain

--- src/contractor/steps/NectarToken.py
+++ src/contractor/steps/NectarToken.py
@@ -1,6 +1,7 @@
 import logging
 
 from contractor.steps import Step
+from contractor.network import Chain
 from itertools import zip_longest
 
 logger = logging.getLogger(__name__)
@@ -13,6 +14,7 @@ class NectarToken(Step):
         contract_config = network.contract_config.get(CONTRACT_NAME, {})
         address = contract_config.get('address')
         users = contract_config.get('users', [])
+        mint = contract_config.get('mint', True)
         mint_amount = contract_config.get('mint_amount', 1000000000 * 10 ** 18)
 
         if address:
@@ -24,12 +26,13 @@ class NectarToken(Step):
         txhash = deployer.transact(deployer.contracts['NectarToken'].functions.enableTransfers())
         network.wait_and_check_transaction(txhash)
 
-        # Take mint requests MINT_STRIDE users at a time
-        for i, group in enumerate(zip_longest(*(iter(users),) * MINT_STRIDE)):
-            group = filter(None, group)
-            txhashes = []
-            for j, user in enumerate(group):
-                logger.info('Minting tokens for user %s: %s', i + j, user)
-                txhashes.append(deployer.transact(deployer.contracts['NectarToken'].functions.mint(user, mint_amount)))
+        if mint and network.chain == Chain.HOMECHAIN:
+            # Take mint requests MINT_STRIDE users at a time
+            for i, group in enumerate(zip_longest(*(iter(users),) * MINT_STRIDE)):
+                group = filter(None, group)
+                txhashes = []
+                for j, user in enumerate(group):
+                    logger.info('Minting tokens for user %s: %s', i + j, user)
+                    txhashes.append(deployer.transact(deployer.contracts['NectarToken'].functions.mint(user, mint_amount)))
 
-            network.wait_and_check_transactions(txhashes)
+                network.wait_and_check_transactions(txhashes)
