7bc83cbb472aeeb73664c54b2a376c4248bec4dd
Maxwell Koo <mjkoo90@gmail.com>
2018-12-14 15:32:46 -0700

    Add in some safety checks from contracts before deploy proceeds and split home and side chain key files

--- src/contractor/steps/NectarToken.py
+++ src/contractor/steps/NectarToken.py
@@ -9,15 +9,21 @@ logger = logging.getLogger(__name__)
 CONTRACT_NAME = 'NectarToken'
 MINT_STRIDE = 10
 
+
 class NectarToken(Step):
     def run(self, network, deployer):
         contract_config = network.contract_config.get(CONTRACT_NAME, {})
-        address = contract_config.get('address')
-        users = contract_config.get('users', [])
+        users = [network.normalize_address(a) for a in contract_config.get('users', [])]
         mint = contract_config.get('mint', True)
         mint_amount = contract_config.get('mint_amount', 1000000000 * 10 ** 18)
 
-        if address:
+        address = None
+        if network.chain == Chain.HOMECHAIN:
+            address = network.normalize_address(contract_config.get('home_address'))
+        elif network.chain == Chain.SIDECHAIN:
+            address = network.normalize_address(contract_config.get('side_address'))
+
+        if address and network.is_contract(address):
             logger.warning('Using already deployed contract for network %s at %s', network.name, address)
             deployer.at(CONTRACT_NAME, address)
         else:
@@ -33,6 +39,7 @@ class NectarToken(Step):
                 txhashes = []
                 for j, user in enumerate(group):
                     logger.info('Minting tokens for user %s: %s', i + j, user)
-                    txhashes.append(deployer.transact(deployer.contracts['NectarToken'].functions.mint(user, mint_amount)))
+                    txhashes.append(
+                        deployer.transact(deployer.contracts['NectarToken'].functions.mint(user, mint_amount)))
 
                 network.wait_and_check_transactions(txhashes)
