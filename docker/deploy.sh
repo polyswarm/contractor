4110215ea6295b7b262e22227572cb75ce229a3b
Maxwell Koo <mjkoo90@gmail.com>
2019-01-03 10:55:17 -0700

    Handle json errors during consul push, remove defunct polyswarmd config items from deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -24,10 +24,10 @@ echo "Deploying to homechain"
 contractor deploy --chain home --network $HOMECHAIN --keyfile $HOMECHAIN_KEYFILE -i consul -o consul/homechain.json
 
 # These configuration options are polyswarmd specific
-if [ -z "$APIKEY_DB_URI" ]; then
-    echo "{\"ipfs_uri\": \"$IPFS_URI\", \"artifact_limit\": $ARTIFACT_LIMIT, \"require_api_key\": false}" > consul/config.json
+if [ -z "$AUTH_URI" ]; then
+    echo "{\"ipfs_uri\": \"$IPFS_URI\", \"artifact_limit\": $ARTIFACT_LIMIT}" > consul/config.json
 else
-    echo "{\"ipfs_uri\": \"$IPFS_URI\", \"artifact_limit\": $ARTIFACT_LIMIT, \"require_api_key\": true, \"db_uri\": \"$APIKEY_DB_URI\"}" > consul/config.json
+    echo "{\"ipfs_uri\": \"$IPFS_URI\", \"artifact_limit\": $ARTIFACT_LIMIT, \"auth_uri\": \"$AUTH_URI\"}" > consul/config.json
 fi
 
 # Push configuration to consul
