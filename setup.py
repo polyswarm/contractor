d506f6b88fa127e3b1da30c26c56d74b0842d9c9
Rob Lathrop <rl@polyswarm.io>
2019-08-12 18:37:21 -0400

    Upgrade some versions, and specify non-test dependencies directly in install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import find_packages, setup
 
 
-def parse_requirements():
-    with open('requirements.txt', 'r') as f:
-        return [r for r in f.read().splitlines() if not r.startswith('git')]
-
-
 setup(
     name='contractor',
     version='0.2.0',
@@ -15,7 +10,22 @@ setup(
     url='https://github.com/polyswarm/contracts',
     license='MIT',
     python_requires='>=3.5,<4',
-    install_requires=parse_requirements(),
+    install_requires=[
+        "click==7.0",
+        "ethereum==2.3.2",
+        "eth-account==0.3.0",
+        "hexbytes==0.1.0",
+        "ipython==7.3.0",
+        "requests==2.21.0",
+        "psycopg2-binary==2.7.6.1",
+        "python-consul==1.1.0",
+        "PyYaml==5.1",
+        "SQLAlchemy==1.3.0",
+        "toposort==1.5",
+        "tabulate==0.8.2",
+        "trezor[ethereum,hidapi]==0.11.4",
+        "web3==4.9.2"
+    ],
     include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
