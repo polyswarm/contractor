5dd9bc78baea732f6454153d0c20fc02cf65ae94
Rob Lathrop <rl@polyswarm.io>
2019-08-13 15:20:32 -0400

    Handle git requirements in parse_requirements

--- setup.py
+++ setup.py
@@ -2,18 +2,29 @@ from setuptools import find_packages, setup
 
 
 test_dependencies = [
-        "coverage==4.5.1",
-        "eth-tester[py-evm]==0.1.0b33",
-        "pycodestyle==2.4.0",
-        "pytest==4.0.0",
-        "pytest-cov==2.6.0",
-        "pytest-xdist==1.26.1",
+        'coverage==4.5.1',
+        'eth-tester[py-evm]==0.1.0b33',
+        'pycodestyle==2.4.0',
+        'pytest==4.0.0',
+        'pytest-cov==2.6.0',
+        'pytest-xdist==1.26.1',
     ]
 
 
 def parse_requirements():
+    result = []
     with open('requirements.txt', 'r') as f:
-        return [r for r in f.read().splitlines() if not r.startswith('git') and r not in test_dependencies]
+        for r in f.read().splitlines():
+            if r in test_dependencies:
+                continue
+            elif r.startswith('git'):
+                url, name = r.split('egg=')
+                result.append('{0} @ {1}'.format(name, url))
+                continue
+
+            result.append(r)
+
+    return result
 
 
 setup(
@@ -25,7 +36,7 @@ setup(
     url='https://github.com/polyswarm/contracts',
     license='MIT',
     python_requires='>=3.5,<4',
-    install_requires=parse_requirements() + ["py-solc @ git+http://github.com/polyswarm/py-solc.git@feature/0-5-3"],
+    install_requires=parse_requirements(),
     include_package_data=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
