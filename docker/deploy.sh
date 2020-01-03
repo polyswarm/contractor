7cc5722be50db48d4ec9f7408994b1b73c6cc9c6
Rob Lathrop <rl@polyswarm.io>
2020-01-03 18:19:30 -0500

    Remove polyswarmd values from consul

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -27,14 +27,6 @@ contractor deploy --chain side --network $SIDECHAIN --keyfile $SIDECHAIN_KEYFILE
 echo "Deploying to homechain"
 contractor deploy --chain home --network $HOMECHAIN --keyfile $HOMECHAIN_KEYFILE -a consul -o consul/homechain.json
 
-# These configuration options are polyswarmd specific
-jq -n \
-    --argjson ipfs_uri "\"$IPFS_URI\"" \
-    --argjson artifact_limit "$ARTIFACT_LIMIT" \
-    --argjson auth_uri $([ -n "$AUTH_URI" ] && echo "\"$AUTH_URI\"" || echo null) \
-    --argjson profiler_enabled $([ -n "$PROFILER_ENABLED" -a "x$PROFILER_ENABLED" != "x0" ] && echo true || echo false) \
-    -f docker/config.jq > consul/config.json
-
 # Push configuration to consul
 if [ ! -z "$CONSUL_URI" ]; then
     echo "Pushing config to consul"
